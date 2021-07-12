1ef445b6075a2a756ebcb673a853b0b77bb2b47f
onionn2411 <87318772+onionn2411@users.noreply.github.com>
2021-07-12 21:17:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'em bé khủng long cutiiiii',
+    introDesc: `anh hỏi em cái này em phải trả lời thật nhé, hum được nói dối đâu`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'em có iu cá heo béo nhiều nhiều hum',
+    desc: 'trả lời thật lòng đó, trả lời sai là muốn ngủ cùng anh rùi kakaka ',
+    btnYes: 'iu nhiều ơi là nhiều',
+    btnNo: 'ko, m là com lợn hloz đáng ghét',
+    question:'sao em bé cuti lại iu anh vậy',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'bởi vì cá heo đz, iu em và em iu anh nhiều nhiều',
+    mess: 'anh cx iêu em nhiều thật là nhiều',
+    messDesc: 'hẹn em 31/7/2021 đặt báo thức thịt anh nha',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/do.hoanganh.357 
 }
